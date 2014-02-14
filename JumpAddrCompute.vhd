145fc91914038c0d487ca87b98750a0d58b9104d
mheide <heide@uni-kassel.de>
2014-02-14 12:30:22 +0100

    working on jump

--- JumpAddrCompute.vhd
+++ JumpAddrCompute.vhd
@@ -4,6 +4,8 @@ use ieee.numeric_std.all;
 
 entity JumpAddrCompute is
 	port(
+		clk_i      : in  std_logic;
+		rst_i      : in  std_logic;
 		jumpAddr_i : in  std_logic_vector(25 downto 0);
 		pc_i       : in  std_logic_vector(31 downto 0);
 		pc_o       : out std_logic_vector(31 downto 0)
@@ -11,10 +13,18 @@ entity JumpAddrCompute is
 end entity JumpAddrCompute;
 
 architecture RTL of JumpAddrCompute is
-	signal pc_high : std_logic_vector(31 downto 0);
-	signal pc      : std_logic_vector(31 downto 0);
+	signal pc_high : std_logic_vector(31 downto 0) := (others => '0');
+	signal pc      : std_logic_vector(31 downto 0) := (others => '0');
 begin
-	pc_high <= std_logic_vector(unsigned(pc_i) + 4);
-	pc <= pc_high(31 downto 28) & jumpAddr_i & "00";
+	process(clk_i, rst_i)
+	begin
+		if (rst_i = '1') then
+			pc_high <= (others => '0');
+			pc      <= (others => '0');
+		elsif rising_edge(clk_i) then
+			pc_high <= std_logic_vector(unsigned(pc_i) + 4);
+			pc      <= pc_high(31 downto 28) & jumpAddr_i & "00";
+		end if;
+	end process;
 	pc_o <= pc;
 end architecture RTL;
---
+++ tb_Jump.vhd
@@ -0,0 +1,53 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tb_Jump is
+end entity tb_Jump;
+
+architecture testbench of tb_Jump is
+	component JumpAddrCompute
+		port(
+			clk_i      : in  std_logic;
+			rst_i      : in  std_logic;
+			jumpAddr_i : in  std_logic_vector(25 downto 0);
+			pc_i       : in  std_logic_vector(31 downto 0);
+			pc_o       : out std_logic_vector(31 downto 0));
+	end component JumpAddrCompute;
+	signal clk      : std_logic := '0';
+	signal rst      : std_logic := '0';
+	constant period : time      := 10 ns;
+
+	signal jumpAddr : std_logic_vector(25 downto 0) := (others => '0');
+	signal pc_in    : std_logic_vector(31 downto 0) := (others => '0');
+	signal pc_out   : std_logic_vector(31 downto 0) := (others => '0');
+begin
+	rst <= '1', '0' after 10 ns;
+	dut : JumpAddrCompute
+		port map(clk_i      => clk,
+			     rst_i      => rst,
+			     jumpAddr_i => jumpAddr,
+			     pc_i       => pc_in,
+			     pc_o       => pc_out);
+
+	clock_generator : process is
+	begin
+		clk <= '0';
+		wait for period / 2;
+		clk <= '1';
+		wait for period / 2;
+	end process clock_generator;
+
+	stimulus : process(clk, rst) is
+	begin
+		if rst = '1' then
+			jumpAddr <= (others => '0');
+			pc_in    <= (others => '0');
+			pc_out   <= (others => '0');
+		elsif rising_edge(clk) then
+			jumpAddr <= "00" & x"000010";
+			pc_in    <= std_logic_vector(unsigned(pc_in) + 4);
+		end if;
+	end process stimulus;
+
+end architecture testbench;
